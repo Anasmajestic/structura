ae2b6a470e28df765dd0cbc4ac1311efc712281b
Anasmajestic <130275740+Anasmajestic@users.noreply.github.com>
2024-08-07 08:46:44 +0530

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+
+
+let menubtn = document.querySelector("#menubtn")
+let menu = document.querySelector("#menu")
+
+menubtn.addEventListener("click",()=>{
+    menu.classList.toggle("hidemenu")
+})
+
