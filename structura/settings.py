c43f3a87bfd81bf8dbc9d0fec7c9d3d14f970d4a
Anasmajestic <130275740+Anasmajestic@users.noreply.github.com>
2024-08-07 08:59:38 +0530

    Update settings.py

--- structura/settings.py
+++ structura/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ["192.168.29.32"]
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app]
 
 
 # Application definition
